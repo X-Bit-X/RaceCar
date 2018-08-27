e000dd2f214aad06f6ea4fe9e4a3576290c24524
Michael Bloch <mchblch@gmail.com>
2018-08-27 21:03:04 +0200

    Add project files.

---
+++ RaceCar/Error.h
@@ -0,0 +1,18 @@
+#ifndef MYERROR
+#define MYERROR
+
+#include <exception>
+#include <string>
+#include <Windows.h>
+
+class Error : public std::exception
+{
+private:
+	std::string m_error;
+public:
+	Error(const std::string &msg)
+		: m_error{ msg } {}
+	const char* what() const noexcept { return m_error.c_str(); }
+};
+
+#endif // !MYERROR
